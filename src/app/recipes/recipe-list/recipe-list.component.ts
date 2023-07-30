afcd0b327e8268c7b9456af38d4573b0482c4c06
AchrafJhidri <achrafjh@gmail.com>
2023-07-30 22:06:39 +0200

    recipe show detail works !

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,10 +6,19 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+
   recipes : Recipe[] = [
     new Recipe("pancakes","Fluffy pancakes","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK62GSZlxhbBCTGP1ldw97wXC4qwa1o2QwbQ&usqp=CAU"),
     new Recipe("beghrir","beghrir mt9oub mziaan","https://static.wixstatic.com/media/94e2e7_8ce40d0ac7dd4c3889f6513b7047af63~mv2.jpg/v1/fill/w_640,h_424,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/94e2e7_8ce40d0ac7dd4c3889f6513b7047af63~mv2.jpg"),
 ]
+  @Output() selectedRecipe : EventEmitter<Recipe> = new EventEmitter(); 
 
+  onSelectRecipe(recipe : Recipe){
+    this.selectedRecipe.emit(recipe);
+  }
+
+  ngOnInit(): void {
+    this.selectedRecipe.emit(this.recipes[0]);
+  }
 }
